7eaea821533f87074d1a8bded53ffe0ad7d282fd
Guilherme Augusto Borges Pinheiro Junior <044748@campus1.uniaraxa.edu.br>
2022-10-25 20:14:58 -0300

    Primeira versao do projeto

---
+++ Solution1/Minicurso_venda.API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Minicurso_venda.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
